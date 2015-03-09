ea1d124333f9d5b44a2c6e589e595dfae9910800
Matthew Kennedy <lispnik@fastmail.fm>
2015-03-08 18:12:00 -0700

    Import code

--- src/clojure_cards/core.clj
+++ src/clojure_cards/core.clj
@@ -1,6 +1,12 @@
-(ns clojure-cards.core)
+(ns clojure-cards.core
+  (:require [clojure.string :refer [join]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn cards
+  ([] (cards (find-ns 'clojure.core)))
+  ([ns]
+   (let [publics (ns-publics ns)]
+     (map (fn [[name symbol]]
+            (let [{:keys [doc arglists]} (meta symbol)]
+              {:front (str name "\n\n" (join "\n" (map str arglists)))
+               :back (if (nil? doc) "(no doc)" doc)}))
+          publics))))
